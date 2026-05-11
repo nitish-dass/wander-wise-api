59ae35ca3389b2f13fc3643d5440d32841ca4a25
nitish-dass <dass2005nd@gmail.com>
2026-05-11 15:01:29 +0545

    Faaahhh

--- handlers/baggage.js
+++ handlers/baggage.js
@@ -4,7 +4,7 @@ import { create } from "../services/baggage.js";
 const BAGGAGE_ROUTER = Router();
 
 BAGGAGE_ROUTER.post("/", async (req, res, next) => {
-    const baggage = await create(req.body);
+    const baggage = await create(req.body);  // create({name: "jacket"})
     res.status(201).json({ data: baggage });
 });
 
--- handlers/index.js
+++ handlers/index.js
@@ -3,6 +3,6 @@ import BAGGAGE_ROUTER from "./baggage.js"
 
 const HANDLERS = Router()
 
-HANDLERS.use("/baggages", BAGGAGE_ROUTER)
+HANDLERS.use("/baggages", BAGGAGE_ROUTER);  // /baggages is an end point(created), when called opens baggage_router
 
 export default HANDLERS;
