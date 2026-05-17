f58e538b7e6ae2371bb75f1b196ca798c57f5f6e
nitish-dass <dass2005nd@gmail.com>
2026-05-17 14:43:05 +0545

    extracted data

--- handlers/baggage.js
+++ handlers/baggage.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { create } from "../services/baggage.js";
+import { create, getAll, getOne } from "../services/baggage.js";
 import { createBaggageValidator } from "../validators/baggage.js";
 
 const BAGGAGE_ROUTER = Router();
@@ -8,6 +8,25 @@ BAGGAGE_ROUTER.post("/", createBaggageValidator, async (req, res, next) => {
     try {
         const baggage = await create(req.body);  // create({name: "jacket"})
         res.status(201).json({ data: baggage });
+    } catch (error) {
+        next(error);
+    }
+});
+
+BAGGAGE_ROUTER.get("/", async (req, res, next) => {
+    try {
+        const baggages = await getAll();
+        res.status(200).json({ data: baggages });
+    } catch (error) {
+        next(error);
+    }
+    
+});
+
+BAGGAGE_ROUTER.get("/:id", async (req, res, next) => {
+    try {
+        const baggage = await getOne(req.params.id);
+          res.status(200).json({ data: baggage });
     } catch (error) {
         next(error);
     }
--- services/baggage.js
+++ services/baggage.js
@@ -4,3 +4,13 @@ export const create = async (data) => {     //(data => { name: "jacket" })
     const baggage = await Baggage.create(data);  
     return baggage;
 }
+
+export const getAll = async () => {
+    const baggages = await Baggage.find();
+    return baggages;
+}
+
+export const getOne = async (_id) => {
+    const baggage = await Baggage.findById(_id);
+    return baggage;
+}
