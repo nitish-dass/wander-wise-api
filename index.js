19b553c4e74249855f94fb56a4d62befdd4fa7b0
nitish-dass <dass2005nd@gmail.com>
2026-07-30 14:55:17 +0545

    cors updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ connectDB();
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIOMS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
